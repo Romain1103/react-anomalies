6b0ae4e45f8f9afa01cf9b68e53192ad5fcdc5b7
galtier <galtier.romainpro@gmail.com>
2021-10-15 14:17:07 +0200

    navigation debut

---
+++ src/components/Navigation.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navigation = () => {
+    return (
+        <div className="navigation">
+            <NavLink exact to="/">
+                Accueil
+            </NavLink>
+            <NavLink exact to="/a-propos">
+                A proposs
+            </NavLink>
+        </div>
+    );
+};
+
+export default Navigation;
---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Navigation />
+            <h1>
+                A propos 
+            </h1>
+            <p>gojengfipz,fpoza,fpoza,fpoa,copezdopa,</p>
+            <p>gojzndoanfoianfoidna</p>
+        </div>
+    );
+};
+
+export default About;
